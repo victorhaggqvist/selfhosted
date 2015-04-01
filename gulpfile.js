f9db7a33ea6afd5d693d0939ac2ebbaf0611332e
Victor Häggqvist <victor@snilius.com>
2015-04-02 00:56:48 +0200

    chore: style things

--- gulpfile.js
+++ gulpfile.js
@@ -4,29 +4,23 @@ var gulp = require('gulp'),
     uglify = require('gulp-uglify'),
     livereload = require('gulp-livereload'),
     runSeq = require('run-sequence'),
-    exec = require('child_process').exec;
-
-var shell = function (cmd) {
-  exec(cmd, function (err, stdout, stderr) {
-    console.log(stdout);
-    console.log(stderr);
-  });
-};
+    deploy = require('gulp-gh-pages'),
+    htmlreplace = require('gulp-html-replace');
 
 gulp.task('style', function () {
-  gulp.src(['./style/*.scss'])
+  return gulp.src(['./style/*.scss'])
     .pipe(sass({
       includePaths: ['./bower_components/bootstrap-sass/assets/stylesheets/bootstrap'],
-      //outputStyle: 'compressed'
+      outputStyle: 'compressed'
     }))
     .pipe(gulp.dest('./dist/css'));
 });
 
 gulp.task('js', function () {
-  gulp.src([
-    'bower_components/jquery/dist/jquery.js',
-    'bower_components/bootstrap-sass/assets/javascripts/bootstrap.js'
-  ])
+  return gulp.src([
+      'bower_components/jquery/dist/jquery.js',
+      'bower_components/bootstrap-sass/assets/javascripts/bootstrap.js'
+    ])
     .pipe(uglify())
     .pipe(gulp.dest('./dist'));
 });
@@ -36,15 +30,19 @@ gulp.task('watch', function () {
   gulp.watch(['./style/*.scss'],['style']);
 });
 
-gulp.task('fontello', function () {
-  shell('fontello-cli --config fontello.json --css fontello/css --font fontello/font install');
+gulp.task('font', function () {
+  return gulp.src('fontello/**/*').pipe(gulp.dest('./dist'));
 });
 
-gulp.task('fonts', function () {
-  return gulp.src('fontello/**/*')
+gulp.task('deploy', function () {
+  return gulp.src('./dist/**/*')
+    .pipe(deploy(options));
+});
+
+gulp.task('index', ['style','js','font'], function () {
+  return gulp.src('./dist/index.html')
+    .pipe(htmlreplace({
+      'css': ['css/bootstrap.css','css/selfont.css']
+    }))
     .pipe(gulp.dest('./dist'));
-});
-
-gulp.task('default', function () {
-  runSeq('style','js');
 });
